f36d06c2065656a21d61ed49a3bcbfb4357ed2e3
luka <lukaonik@gmail.com>
2026-02-10 11:34:58 +0400

    docs: update typescript md

--- playground/src/vite-env.d.ts
+++ playground/src/vite-env.d.ts
@@ -1,19 +1,3 @@
 /// <reference types="vite/client" />
+/// <reference types="../../src/client" />
 
-declare module "*?oh" {
-  import type { ImageSrc } from "../../src/react/types";
-  const src: ImageSrc;
-  export default src;
-}
-
-declare module "*?oh&placeholder=true" {
-  import type { ImageSrc } from "../../src/react/types";
-  const src: ImageSrc;
-  export default src;
-}
-
-declare module "*?oh&placeholder=true&blur=10" {
-  import type { ImageSrc } from "../../src/react/types";
-  const src: ImageSrc;
-  export default src;
-}
