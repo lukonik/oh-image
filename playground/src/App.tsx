6a220f001a3a7a3d113ed0ddc2a2b70e06e13848
Luka Onikadze <81145822+lukonik@users.noreply.github.com>
2026-02-12 14:29:09 +0400

    feat: size and placeholder use sensible defaults (#20)
    * use loading="lazy" by default
    * if sizes is not provided for specific cases use sizes="auto"

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -5,7 +5,7 @@ import helloImage from "./hello.png?oh&placeholder=true";
 export function App() {
   return (
     <div>
-      <Image src={helloImage} style={{width:'400px',height:'400px'}} alt="forest image" />;
+      <Image src={helloImage} style={{width:'400px',height:'400px'}} asap={true} loading="lazy" alt="forest image" />;
       <Image src={forestImage} alt="forest image" />;
     </div>
   );
