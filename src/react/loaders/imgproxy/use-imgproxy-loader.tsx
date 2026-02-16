0153cd8e410b5fe653c28fdca259ea0cea70fefa
luka onikadze <lukaonik@gmail.com>
2026-02-16 19:26:56 +0400

    fix(imgproxy): allow undefined to loader hooks

--- src/react/loaders/imgproxy/use-imgproxy-loader.tsx
+++ src/react/loaders/imgproxy/use-imgproxy-loader.tsx
@@ -3,12 +3,12 @@ import { createImgproxyUrl } from "./create-imgproxy-url";
 import { useImgproxyContext } from "./imgproxy-context";
 import type { ImgproxyOptions } from "./imgproxy-options";
 
-export function useImgproxyLoader(options: ImgproxyOptions) {
+export function useImgproxyLoader(options?: ImgproxyOptions) {
   const context = useImgproxyContext();
-  const path = options.path || context.path;
+  const path = options?.path || context.path;
   const transforms = {
     ...context.transforms,
-    ...options.transforms,
+    ...options?.transforms,
   };
   return (imageOptions: ImageLoaderOptions) =>
     createImgproxyUrl(path, transforms, imageOptions);
--- src/react/loaders/imgproxy/use-imgproxy-placeholder.tsx
+++ src/react/loaders/imgproxy/use-imgproxy-placeholder.tsx
@@ -3,12 +3,12 @@ import { createImgproxyUrl } from "./create-imgproxy-url";
 import { useImgproxyContext } from "./imgproxy-context";
 import type { ImgproxyOptions } from "./imgproxy-options";
 
-export function useImgproxyPlaceholder(options: ImgproxyOptions) {
+export function useImgproxyPlaceholder(options?: ImgproxyOptions) {
   const context = useImgproxyContext();
-  const path = options.path || context.path;
+  const path = options?.path || context.path;
   const transforms = {
     ...context.placeholderTransforms,
-    ...options.transforms,
+    ...options?.transforms,
   };
   return (imageOptions: ImageLoaderOptions) =>
     createImgproxyUrl(path, transforms, imageOptions);
