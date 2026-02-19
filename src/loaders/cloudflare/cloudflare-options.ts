98541fe6aea724bf943c4e0aeec190413f8d793a
luka <lukaonik@gmail.com>
2026-02-19 20:34:06 +0400

    docs: kontent update

--- src/loaders/cloudflare/cloudflare-options.ts
+++ src/loaders/cloudflare/cloudflare-options.ts
@@ -1,37 +1,35 @@
 import type {
   BaseGlobalLoaderOptions,
-  BaseLoaderOptions,
-  BaseLoaderTransforms,
+  BaseLoaderOptions
 } from "../base-loader-options";
 
-export type CloudflareTransforms = BaseLoaderTransforms &
-  Partial<{
-    anim: boolean;
-    background: string;
-    blur: number;
-    brightness: number;
-    compression: "fast";
-    contrast: number;
-    dpr: number;
-    flip: "h" | "v" | "hv";
-    fit: "scale-down" | "contain" | "cover" | "crop" | "pad" | "squeeze";
-    format: "auto" | "avif" | "webp" | "jpeg" | "baseline-jpeg";
-    gamma: number;
-    gravity: "auto" | "left" | "right" | "top" | "bottom" | string;
-    height: number;
-    width: number;
-    maxWidth: number;
-    metadata: "keep" | "copyright" | "none";
-    quality: number | "high" | "medium-high" | "medium-low" | "low";
-    rotate: number;
-    sharpen: number;
-    trim: [number, number, number, number];
-    zoom: number;
-    saturation: number;
-    segment: "foreground";
-    onerror: "redirect";
-    "slow-connection-quality": number;
-  }>;
+export type CloudflareTransforms = Partial<{
+  anim: boolean;
+  background: string;
+  blur: number;
+  brightness: number;
+  compression: "fast";
+  contrast: number;
+  dpr: number;
+  flip: "h" | "v" | "hv";
+  fit: "scale-down" | "contain" | "cover" | "crop" | "pad" | "squeeze";
+  format: "auto" | "avif" | "webp" | "jpeg" | "baseline-jpeg";
+  gamma: number;
+  gravity: "auto" | "left" | "right" | "top" | "bottom" | string;
+  height: number;
+  width: number;
+  maxWidth: number;
+  metadata: "keep" | "copyright" | "none";
+  quality: number | "high" | "medium-high" | "medium-low" | "low";
+  rotate: number;
+  sharpen: number;
+  trim: [number, number, number, number];
+  zoom: number;
+  saturation: number;
+  segment: "foreground";
+  onerror: "redirect";
+  "slow-connection-quality": number;
+}>;
 
 export type CloudflareOptions = BaseLoaderOptions<CloudflareTransforms>;
 export type CloudflareGlobalOptions =
