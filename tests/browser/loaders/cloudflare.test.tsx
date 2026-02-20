d172a9a6e3422197925027c8817b7136c62520d8
luka <lukaonik@gmail.com>
2026-02-20 12:28:04 +0400

    test: cloudflare

--- tests/browser/loaders/cloudflare.test.tsx
+++ tests/browser/loaders/cloudflare.test.tsx
@@ -1,8 +1,7 @@
 import {
-    createAnyDescribeTest,
-    createBooleanDescribeTest,
-    createNumberDescribeTest,
-    createStringDescribeTest,
+  describeBooleanOption,
+  describeImageOptions,
+  describeOptionFactory,
 } from "./loaders-utils";
 import type { CloudflareTransforms } from "../../../src/loaders/cloudflare/cloudflare-options";
 import { useCloudflareLoader } from "../../../src/loaders/cloudflare/cloudflare-loader";
@@ -12,53 +11,51 @@ chai.config.truncateThreshold = 100000;
 describe("cloudflare", () => {
   const optionSeparator = "=";
   const paramSeparator = ",";
-  let booleanDescribe = createBooleanDescribeTest<CloudflareTransforms>(
+
+  const describeOption = describeOptionFactory<CloudflareTransforms>(
     (options) => useCloudflareLoader(options),
-    paramSeparator,
+    optionSeparator,
+    ";",
+  );
+
+  describeImageOptions(
+    () =>
+      useCloudflareLoader({
+        path: "http://contentful.com",
+      }),
+    "width",
+    "height",
+    optionSeparator,
+  );
+
+  describeBooleanOption(
+    (options) => useCloudflareLoader(options),
     optionSeparator,
     true,
   );
 
-  let numberDescribe = createNumberDescribeTest<CloudflareTransforms>(
-    (options) => useCloudflareLoader(options),
-    paramSeparator,
-    optionSeparator,
-  );
-
-  let stringDescribe = createStringDescribeTest<CloudflareTransforms>(
-    (options) => useCloudflareLoader(options),
-    paramSeparator,
-    optionSeparator,
-  );
-
-  let anyDescribe = createAnyDescribeTest<CloudflareTransforms>(
-    (options) => useCloudflareLoader(options),
-    paramSeparator,
-    optionSeparator,
-  );
-
-  booleanDescribe("anim");
-  stringDescribe("background", "black");
-  numberDescribe("blur", 120);
-  numberDescribe("brightness", 77);
-  stringDescribe("compression", "fast");
-  numberDescribe("contrast", 55);
-  numberDescribe("dpr", 1);
-  stringDescribe("fit", "scale-down");
-  stringDescribe("flip", "h");
-  stringDescribe("format", "baseline-jpeg");
-  numberDescribe("gamma", 5);
-  stringDescribe("gravity", "top");
-  numberDescribe("height", 120);
-  stringDescribe("metadata", "copyright");
-  stringDescribe("onerror", "redirect");
-  stringDescribe("quality", "medium-high");
-  numberDescribe("quality", 50);
-  numberDescribe("rotate", 180);
-  numberDescribe("saturation", 10);
-  stringDescribe("segment", "foreground");
-  numberDescribe("sharpen", 500);
-  numberDescribe("slow-connection-quality", 50);
-  numberDescribe("zoom", 100);
-  anyDescribe("trim", [24, 27, 16, 14], "24;27;16;14");
+  describeOption("anim", true);
+  describeOption("background", "black");
+  describeOption("blur", 120);
+  describeOption("brightness", 77);
+  describeOption("compression", "fast");
+  describeOption("contrast", 55);
+  describeOption("dpr", 1);
+  describeOption("fit", "scale-down");
+  describeOption("flip", "h");
+  describeOption("format", "baseline-jpeg");
+  describeOption("gamma", 5);
+  describeOption("gravity", "top");
+  describeOption("height", 120);
+  describeOption("metadata", "copyright");
+  describeOption("onerror", "redirect");
+  describeOption("quality", "medium-high");
+  describeOption("quality", 50);
+  describeOption("rotate", 180);
+  describeOption("saturation", 10);
+  describeOption("segment", "foreground");
+  describeOption("sharpen", 500);
+  describeOption("slow-connection-quality", 50);
+  describeOption("zoom", 100);
+  describeOption("trim", [24, 27, 16, 14]);
 });
