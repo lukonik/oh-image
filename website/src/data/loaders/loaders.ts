2f69dc312fbc976c692e2b942a7f69926dcdfb0a
luka <lukaonik@gmail.com>
2026-02-25 11:33:50 +0400

    docs: add llm generation

--- website/src/data/loaders/loaders.ts
+++ website/src/data/loaders/loaders.ts
@@ -1,5 +1,4 @@
 import { cloudflare } from "./cloudflare";
-import { cloudinary } from "./cloudinary";
 import { contentful } from "./contentful";
 import { imgproxy } from "./imgproxy";
 import { kontent } from "./kontent";
